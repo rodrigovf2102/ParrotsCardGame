39ce1742fd2cf3db92c90783c69906900394c91b
rodrigovf2102 <rodrigovf2102@gmail.com>
2022-06-15 00:48:12 -0300

    cartas com frente e verso adicionadas, distribuidas aleatoriamente e trocam de lado quando clica

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let qntdeCarta=0;
+let cartasJogo =[];
 
 while(!qntdeCarta){
     qntdeCarta = prompt("Digite o número de cartas em jogo: (4 a 14)");
@@ -8,6 +9,24 @@ while(!qntdeCarta){
 }
 
 const caixaJogo = document.querySelector(".caixa-jogo")
+for(let i=1;i<=qntdeCarta;i++){
+    tipoDeCarta = Math.ceil(i/2);
+    cartasJogo[i-1] = tipoDeCarta;
+}
+cartasJogo.sort(comparador);
+
 for(let i=0;i<qntdeCarta;i++){
-    caixaJogo.innerHTML = caixaJogo.innerHTML+ `<div class='carta-jogo'><img src='images/front.png'></div>`;
+    caixaJogo.innerHTML = caixaJogo.innerHTML+ 
+    `<div class='carta-jogo' onclick="virarCarta(this)">
+        <img class="frente" src='images/front.png'>
+        <img class="verso escondido" src='images/${cartasJogo[i]}.gif'>
+    </div>`;
+}
+
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+function virarCarta(carta){
+    carta.querySelector(".frente").classList.add("escondido");
+    carta.querySelector(".verso").classList.remove("escondido"); 
 }
